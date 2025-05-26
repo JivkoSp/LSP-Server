3f50142ed2bd701b8e15c4a861aedbfa676f57af
JivkoSp <atjspasov@gmail.com>
2025-05-26 12:54:06 +0300

    Updated server/src/methods/initialize.ts

--- server/src/methods/initialize.ts
+++ server/src/methods/initialize.ts
@@ -1,3 +1,5 @@
+import { RequestMessage } from "../server";
+
 type ServerCapabilities = Record<string, unknown>;
 
 interface InitializeResult {
@@ -7,3 +9,21 @@ interface InitializeResult {
 		version?: string;
 	};
 }
+
+export const initialize = (message: RequestMessage):
+        InitializeResult => {
+            
+            let capabilities = {
+                
+            };
+            
+            let serverInfo = {
+                name: "LSP SERVER",
+                version: "0.0.1"
+            };
+
+            return {
+                capabilities: capabilities,
+                serverInfo: serverInfo
+            };
+        };
