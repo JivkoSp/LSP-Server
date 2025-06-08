d0b5fb2a0c2a9d9d6644f09fb43c782aa2bc47f4
JivkoSp <atjspasov@gmail.com>
2025-06-08 15:00:00 +0300

    Added method lookup for message in sever.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,40 +11,61 @@ export interface RequestMessage extends Message {
 	params?: unknown[] | object;
 }
 
+type RequestMethod = (message: RequestMessage) => object;
+
+const methodLookup: Record<string, RequestMethod> = {
+  initialize,
+};
+
+const respond = (id: RequestMessage['id'], result: object) => {
+  
+  const message = JSON.stringify({id, result});
+  const messageLength = Buffer.byteLength(message, "utf-8");
+  const header = `Content-Length: ${messageLength}\r\n\r\n`;
+
+  log.write(header + message);
+  process.stdout.write(header + message);
+
+
+};
+
 let buffer = "";
 
 process.stdin.on("data", (chunk) => {
+  
+  buffer += chunk;
 
-  log.write(chunk);
+  while(true) {
+    // Check for the content length line
+    const lengthMatch = buffer.match(/Content-Length: (\d+)\r\n/);
 
+    // Check if the message is full
+    if(!lengthMatch) {
+       break; // Continue to add to the buffer
+    }
 
-  // buffer += chunk;
-  // while(true) {
-  //   // Check for the content length line
-  //   const lengthMatch = buffer.match(/Content-Length: (\d+)\r\n/);
+    const contentLength = parseInt(lengthMatch[1], 10);
 
-  //   // Check if the message is full
-  //   if(!lengthMatch) {
-  //      break; // Continue to add to the buffer
-  //   }
+    const messageStart = buffer.indexOf("\r\n\r\n") + 4;
 
-  //   const contentLength = parseInt(lengthMatch[1], 10);
-  //   const messageStart = buffer.indexOf("\r\n\r\n") + 4;
+    // Continue unless the full message is in the buffer
+    if(buffer.length < messageStart + contentLength) {
+      break;
+    }
 
-  //   // Continue unless the full message is in the buffer
-  //   if(buffer.length < messageStart + contentLength) {
-  //     break;
-  //   }
+    const rawMessage = buffer.slice(messageStart, messageStart + contentLength);
 
-  //   const rawMessage = buffer.slice(messageStart, messageStart + contentLength);
-  //   const message = JSON.parse(rawMessage);
+    const message = JSON.parse(rawMessage);
 
-  //   log.write({id: message.id, method: message.method});
+    log.write({id: message.id, method: message.method});
 
-  //   // TODO: Call method and respond
+    const method = methodLookup[message.method];
 
+    if(method) {
+      respond(message.id, method(message));
+    }
 
-  //   // Remove the processed message from the buffer
-  //   buffer = buffer.slice(messageStart + contentLength);
-  // } 
+    // Remove the processed message from the buffer
+    buffer = buffer.slice(messageStart + contentLength);
+  } 
 });
